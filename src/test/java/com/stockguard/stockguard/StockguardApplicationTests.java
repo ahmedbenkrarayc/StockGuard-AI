fb3d928edae81687ea91ac28c21608358ec54609
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-12-22 16:02:23 +0100

    project structure setup

---
+++ src/test/java/com/stockguard/stockguard/StockguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockguard.stockguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
