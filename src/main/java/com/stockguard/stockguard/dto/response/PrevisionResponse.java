628d1d21db6287a106eb7f1016471206e4660193
Saad_elquaul <saadelquaul@gmail.com>
2025-12-30 12:02:32 +0100

    Créer la classe PrevisionResponse pour encapsuler les données de réponse des prévisions

---
+++ src/main/java/com/stockguard/stockguard/dto/response/PrevisionResponse.java
@@ -0,0 +1,21 @@
+package com.stockguard.stockguard.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PrevisionResponse {
+    private Long id;
+    private String produitNom;
+    private String entrepotNom;
+    private LocalDate datePrevision;
+    private Integer quantitePrevue30Jours;
+    private Integer niveauConfiance;
+    private String recommandation;
+}
